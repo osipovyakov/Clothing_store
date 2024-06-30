73226c4ad5b0c87317a6a865bb99dbe3c0b5abc9
osipovyakov <110915696+osipovyakov@users.noreply.github.com>
2024-06-30 23:36:05 +0700

    frontend

--- my_shop/users/views.py
+++ my_shop/users/views.py
@@ -5,5 +5,5 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('/')
+    success_url = reverse_lazy('shop:get_product_list')
     template_name = 'users/users/signup.html' 
